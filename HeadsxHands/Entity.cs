1c14d1056f1e516e5b0ce697111d5b13296bf678
Ivan <kulkov_2003@bk.ru>
2023-09-25 14:50:03 +0300

    added fields to the Entity class and methods for working with the health field

--- HeadsxHands/Entity.cs
+++ HeadsxHands/Entity.cs
@@ -8,27 +8,124 @@ namespace HeadsxHands
 {
     internal class Entity
     {
-        private int _damage;
+        private int _attack;
         private int _protection;
         private int _health;
+        private int _damage;
+        private int _maxHealth;
 
-        public int Damage { get { return _damage;} }
-        public int Protection { get { return _protection;} }
-        public int Health { get { return _health;} }
+        private int _amountOfHealing = 4;
+        private bool _isDead = false;
+        public int Damage { get { return _damage; } }
+
+        public bool IsDead
+        {
+            get
+            {
+                return _isDead;
+            }
+        }
+        public int Attack
+        {
+            get
+            {
+                return _attack;
+            }
+
+            set
+            {
+                if (value > 0 && value < 31)
+                    {
+                    try
+                    {
+                        _attack = value;
+                    }
+                    catch
+                    {
+                        _attack = 1;
+                    }
+                }
+                else
+                {
+                    _attack = 1;
+                }
+            }
+        }
+
+        public int Protection
+        {
+            get
+            {
+                return _protection;
+            }
+
+            set
+            {
+                if (value > 0 && value < 31)
+                {
+                    try
+                    {
+                        _protection = value;
+                    }
+                    catch
+                    {
+                        _protection = 1;
+                    }
+                }
+                else
+                {
+                    _protection = 1;
+                }
+            }
+        }
+
+        public int Health
+        {
+            get
+            {
+                return _health; 
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    Died();
+                    _health = value;
+                }
+                else
+                {
+                    _health = value;
+                }
+            }
+        }
 
         public Entity()
         {
-            this._damage = 0;
-            this._protection = 0;
-            this._health = 0;
+            this.Attack = 0;
+            this.Protection = 1;
+            this._health = 1;
+            this._maxHealth = this._health;
+            this._damage = 1;
         }
 
-        public Entity(int damage, int protection, int health)
+        public Entity(int attack, int protection, int health, int damage)
         {
-            this._damage = damage;
-            this._protection = protection;
+            this.Attack = attack;
+            this.Protection = protection;
             this._health = health;
+            this._maxHealth = this._health;
+            this._damage = damage;
         }
 
+        public void Died()
+        {
+            this._isDead = true;
+        }
+
+        public void Heal()
+        {
+            this.Health = (int)(this._maxHealth * 0.3);
+        }
     }
 }
--- HeadsxHands/Program.cs
+++ HeadsxHands/Program.cs
@@ -7,6 +7,13 @@ namespace HeadsxHands // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Entity man = new Entity(31, 1, 5, 5);
+            for(int i = 0; i < 5; i++)
+            {
+                man.Health -= 1;
+                Console.WriteLine(man.Health);
+                Console.WriteLine(man.IsDead);
+            }
         }
     }
 }
