763bb34a0f8b31ff9ba96f0b9b3e573ff7786ce3
Ivan <kulkov_2003@bk.ru>
2023-09-25 11:41:53 +0300

    Add basic classes and Entity class structure created

---
+++ HeadsxHands/Entity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadsxHands
+{
+    internal class Entity
+    {
+        private int _damage;
+        private int _protection;
+        private int _health;
+
+        public int Damage { get { return _damage;} }
+        public int Protection { get { return _protection;} }
+        public int Health { get { return _health;} }
+
+        public Entity()
+        {
+            this._damage = 0;
+            this._protection = 0;
+            this._health = 0;
+        }
+
+        public Entity(int damage, int protection, int health)
+        {
+            this._damage = damage;
+            this._protection = protection;
+            this._health = health;
+        }
+
+    }
+}
