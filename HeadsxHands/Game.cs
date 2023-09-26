ce5e242b79f1133b893926d0ff4234db8db2cb8f
Ivan <kulkov_2003@bk.ru>
2023-09-26 15:00:57 +0300

    wrote a method to check the input number for range and type

--- HeadsxHands/Game.cs
+++ HeadsxHands/Game.cs
@@ -9,5 +9,25 @@ namespace HeadsxHands
     internal class Game
     {
 
+
+        private int GetIntInput(int min, int max)
+        {
+            int value;
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out value) && value >= min && value <= max)
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine($"Некорректный ввод. Попробуйте снова. " +
+                                      $"Число должно быть в диапазоне ({min}, {max})");
+                }
+            }
+        }
     }
+
+
+
 }
