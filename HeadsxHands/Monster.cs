edb1f6106d446265cad0770599676d11432c4d65
Ivan <kulkov_2003@bk.ru>
2023-10-02 17:44:28 +0300

    add monster constructor

--- HeadsxHands/Monster.cs
+++ HeadsxHands/Monster.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace HeadsxHands
 {
@@ -18,6 +19,10 @@ namespace HeadsxHands
     {
         public MonsterType Type { get; }
 
-
+        public Monster(MonsterType type, int attack, int protection, int health, (int Min, int Max) damage)
+        : base(attack, protection, health, damage)
+        {
+            Type = type;
+        }
     }
 }
